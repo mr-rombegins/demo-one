820fb92e9758ffa85cd4205055e5fa31e1fad7bb
Victor Kodzo Seayrom <mrseayrom@gmail.com>
2022-03-17 14:34:32 +0000

    my demo

---
+++ JS/script.js
@@ -0,0 +1,36 @@
+let firstName = "Victor";
+let lastName = "Seayrom";
+let age = "22";
+console.log(
+  "My name is " +
+    firstName +
+    " " +
+    lastName +
+    ". I am " +
+    age +
+    " " +
+    "years old."
+);
+console.log(
+  `My name is ${firstName} ${lastName}. I am ${age} years old today.`
+);
+let fruits = ["Mango", "Orange", "Banana", "Apple"];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits.push("Guava"));
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits.unshift("Pear"));
+console.log(fruits);
+
+let i = 0;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+while (i < 10);
+{
+  console.log(i);
+  i++;
+}
